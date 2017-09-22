70879b3eac2703650137a713381c3db9b22ea445
Jens Lechtenboerger <jens.lechtenboerger@fsfe.org>
2017-09-22 20:41:34 +0200

    Add install.el

---
+++ install.el
@@ -0,0 +1,16 @@
+;; If you use the Docker image specified in .gitlab-ci.yml,
+;; this file is not necessary as the image contains the packages
+;; installed here.
+
+(defun install ()
+  "Install prerequisites from ELPA"
+  (package-initialize)
+  ;; org-ref is available on melpa:
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (package-refresh-contents)
+  ;; Emacs comes with an outdated version of org.
+  ;; Need to be more specific to install newer version:
+  (package-install (cadr (assq 'org package-archive-contents)))
+  (package-install 'htmlize)
+  (package-install 'org-ref)
+  )
