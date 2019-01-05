bb17776ff457ac975413fb4f21aeec2c999f12ee
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-01-05 13:26:58 +0100

    Reference issue with failing org-reveal installation

--- emacs-reveal.el
+++ emacs-reveal.el
@@ -41,8 +41,11 @@
 ;; browsers, also mobile and offline.
 ;;
 ;; Emacs-reveal grew out of a forked version of org-reveal whose
-;; development seems to have stalled.  Please see there for more
-;; information:
+;; development seems to have stalled and which cannot be installed
+;; with recent versions of Org mode any more:
+;; https://github.com/yjwen/org-reveal/issues/342
+;; I tried to contribute to org-reveal but did not receive a response
+;; from the maintainer:
 ;; https://github.com/yjwen/org-reveal/issues/349
 ;; https://github.com/yjwen/org-reveal/pull/277
 ;;
