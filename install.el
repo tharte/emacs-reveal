52c5550978cb54bb2497f924f3580df613d68776
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-03-23 14:21:34 +0100

    Install oer-reveal as dependency

---
+++ install.el
@@ -0,0 +1,13 @@
+;; Copyright (C) 2017-2019 Jens Lechtenbörger
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; If you use the Docker image specified in .gitlab-ci.yml,
+;; this file is not necessary as the image contains oer-reveal.
+
+(defun install ()
+  "Install oer-reveal from MELPA"
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-install 'oer-reveal)
+  )
