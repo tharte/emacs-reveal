319f083d43dc91c57074ab481ba444ba419b5ade
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-03-26 08:39:59 +0100

    Update commentary

--- emacs-reveal.el
+++ emacs-reveal.el
@@ -29,8 +29,7 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
-;; TODO
-;; The software emacs-reveal allows to create HTML presentations (with
+;; The software emacs-reveal helps to create HTML presentations (with
 ;; audio explanations if you wish) with reveal.js from Org mode files
 ;; in GNU Emacs with org-re-reveal (a fork of org-reveal) and several
 ;; reveal.js plugins.  Generated presentations are usable with standard
@@ -67,48 +66,29 @@
 ;;
 ;; Emacs-reveal is only available from GitLab.
 ;;
-;; Create a directory for your presentation(s)' Org source files.  In
-;; that directory:
+;; Create a directory for your presentation(s)' Org source files.
+;; Somewhere, maybe in that directory install emacs-reveal and its
+;; prerequisite packages:
 ;; (a) git clone https://gitlab.com/oer/emacs-reveal.git
 ;; (b) cd emacs-reveal
-;; (c) Make sure that Emacs package org-re-reveal-ref is installed.
+;; (c) Make sure that Emacs packages org-re-reveal-ref and oer-reveal
+;;     are installed.
 ;;     - Install with the usual package mechanism or like
 ;;       this: emacs --batch --load install.el --funcall install
-;;     - Or you could use this docker image:
-;;       registry.gitlab.com/oer/docker/debian-emacs-tex-org:v3.3
-;; (d) Add a line like this to ~/.emacs:
+;;     - Or you could use this Docker image:
+;;       registry.gitlab.com/oer/docker/emacs-reveal:1.1
+;; (d) Add a line like this to ~/.emacs (beware, this changes settings
+;;     of other packages) or to your publication code:
 ;;     (load "/path/to/emacs-reveal/emacs-reveal.el")
-;; (e) Emacs-reveal suggests that reveal.js and several of its plugins
-;;     are installed in the directory specified by customizable
-;;     variable `emacs-reveal-submodules-dir'.
-;;     Upon first loading in Emacs, emacs-reveal offers to download
-;;     necessary software via Git for you.
-;;     Also, (aiming for installation via MELPA one day) you may
-;;     customize `emacs-reveal-generate-org-includes-p' to
-;;     generate include files for shipped Org config files in the
-;;     stable location specified by `emacs-reveal-org-includes-dir'.
 ;;
 ;; * Usage
 ;; Please check out the emacs-reveal howto mentioned above.  In
-;; particular, the howto makes use of emacs-reveal-publish.el to
-;; publish reveal.js presentations from Org source files (in file
-;; elisp/publish.el).
+;; particular, the howto contains a sample file "elisp/publish.el" to
+;; publish reveal.js presentations from Org source files.
 ;; Note that the HTML version of the howto is generated in a Docker
 ;; image on GitLab; its YML configuration shows the necessary steps
 ;; to generate and publish the project:
 ;; https://gitlab.com/oer/emacs-reveal-howto/blob/master/.gitlab-ci.yml
-;;
-;; Variable `emacs-reveal-dir' points to the directory of emacs-reveal
-;; and its embedded plugins and resources.  You may want to use that
-;; variable in your own publication code (similarly to its use in
-;; elisp/publish.el of the howto).  Note that subdirectory
-;; "title-slide" contains some variants for title slides of
-;; presentations, and subdirectory "css" contains sample CSS.
-;; Subdirectory "org" contains Org files to embed in presentations.
-;; Please be warned that included resources, in particular
-;; css/jl-simple.css, may change in incompatible ways.
-;; You may want to work with your own copies.
-;;
 
 ;;; Code:
 (require 'oer-reveal-publish)
