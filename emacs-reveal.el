49d2cd26d9560b042ad31b5bf1b74ce5148b785d
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-10-24 14:49:03 +0200

    Version 4.2.0

--- emacs-reveal.el
+++ emacs-reveal.el
@@ -7,7 +7,7 @@
 
 ;; Author: Jens Lechtenbörger
 ;; URL: https://gitlab.com/oer/emacs-reveal
-;; Version: 4.1.0
+;; Version: 4.2.0
 ;; Package-Requires: ((emacs "24.4") (oer-reveal "1.4.0") (org-re-reveal-ref "0.9.1"))
 ;; Keywords: hypermedia, tools, slideshow, presentation, OER
 
