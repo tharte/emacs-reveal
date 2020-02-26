cf47ac40e65ed9960757720ae4e1932d92d44685
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2020-02-26 17:40:31 +0100

    Add test file

---
+++ tests/elisp/publish.el
@@ -0,0 +1,28 @@
+;;; publish.el --- Publish reveal.js presentation from Org file
+;; -*- Mode: Emacs-Lisp -*-
+;; -*- coding: utf-8 -*-
+
+;; SPDX-FileCopyrightText: 2020 Jens Lechtenbörger
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;;; Commentary:
+;; To create reveal.js presentations for the Org mode test cases,
+;; use this file from its parent directory with the following shell
+;; command:
+;; emacs --batch --load elisp/publish.el
+
+;;; Code:
+(package-initialize)
+
+;; Load emacs-reveal.
+(add-to-list 'load-path
+             (expand-file-name "../.." (file-name-directory load-file-name)))
+(require 'emacs-reveal)
+
+;; Don't create PDF.
+(setq oer-reveal-publish-org-publishing-functions
+      '(oer-reveal-publish-to-reveal))
+
+;; Publish Org files.
+(oer-reveal-publish-all)
+;;; publish.el ends here
